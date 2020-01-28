47f1f38be20c910165357ac7935e9ba7bcbbdb97
WilliamMelgoza <wmelgoza@protonmail.com>
2020-01-27 18:53:02 -0600

    connect to the database

--- app.js
+++ app.js
@@ -11,8 +11,12 @@ var app = express();
 
 var config = require('./config.dev');
 
+var mongoose = require('mongoose');
+
+//Connect to MongoDB
+mongoose.connect(config.mongodb, { useNewUrlParser: true });
 //Test the file
-console.log(config);
+// console.log(config);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
