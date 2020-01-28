6346f26b54c0270a0e07c8afd9ec67bf92a876c3
WilliamMelgoza <wmelgoza@protonmail.com>
2020-01-27 20:27:12 -0600

    Create a new user record

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,15 @@ var express = require('express');
 var router = express.Router();
 var Users = require('../../models/users');
 
+router.get('/', function(req, res, next) {
+  Users.find({},function(err, users){
+    if(err){
+     return res.json({'success':false, 'error': err});
+   }
+    return res.json({'success':true, 'users': users});
+  });
+});
+
 router.get('/:userId', function(req,res){
   
   var userId = req.params.userId;
@@ -12,5 +21,22 @@ router.get('/:userId', function(req,res){
      return res.json({'success':true, 'user': user});
    });
  });
+ 
+ router.post('/', function(req, res) {
+  Users.create(new Users({
+    username: req.body.username,
+    email: req.body.email,
+    first_name: req.body.first_name,
+    last_name: req.body.last_name
+  }), function(err, user){
+    
+    if(err){
+      return res.json({success: false, user: req.body, error: err});
+    }
+
+    return res.json({success: true, user: user});
+    
+  });
+});
 
 module.exports = router;
