3bfc38133e9b286c776bdb61d7a1020ded1502f5
WilliamMelgoza <wmelgoza@protonmail.com>
2020-02-03 20:19:15 -0600

    Add logout methods

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -12,18 +12,18 @@ router.post('/register', function(req,res,next){
     email: data.email,
     first_name: data.first_name,
     last_name: data.last_name
-  }), 
-  data.password, 
+  }),
+  data.password,
   function(err, user){
 
     if(err){
 
       return res.json({
-        success: false, 
-        user: req.body, 
+        success: false,
+        user: req.body,
         errors: err
       });
-      
+
     }
 
     return res.json({
@@ -37,26 +37,26 @@ router.post('/register', function(req,res,next){
 router.post('/login', function(req, res, next) {
     //
     passport.authenticate('local', function(err, user, info) {
-  
-      if (err) { 
+
+      if (err) {
         return res.json({success:false, error: err});
       }
-  
+
       if (!user) {
         return res.json({success:false, error: info.message });
       }
-  
+
       req.logIn(user, function(err) {
-  
-        if (err) { 
+
+        if (err) {
           return res.json({success:false, error: err });
         }
-  
+
         //we will use a console.log() to test the session data
         console.log(req.session);
-  
+
         return res.json({success:true, user: user });
-  
+
       });
     })(req, res, next);
   });
