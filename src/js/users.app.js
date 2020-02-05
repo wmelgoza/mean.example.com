909e2703718945438f4a1e30201f5f409cef47b9
WilliamMelgoza <wmelgoza@protonmail.com>
2020-02-05 17:35:53 -0600

    Get the data for the selected user

--- src/js/users.app.js
+++ src/js/users.app.js
@@ -144,6 +144,25 @@ var usersApp = (function() {
     }
   }
 
+  function editUser(id){
+
+    let uri = `${window.location.origin}/api/users/${id}`;
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', uri);
+
+    xhr.setRequestHeader(
+      'Content-Type',
+      'application/json; charset=UTF-8'
+    );
+
+    xhr.send();
+
+    xhr.onload = function(){
+      let data = JSON.parse(xhr.response);
+      console.log(data);
+    }
+  }
+
   function postRequest(formId, url){
     let form = document.getElementById(formId);
     form.addEventListener('submit', function(e){
